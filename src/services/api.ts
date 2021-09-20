2b655ba2d9403226cbbbac28d330fa9feb854e38
Eduardo Augusto de Assis Pinheiro <edspinheiro@gmail.com>
2021-09-20 02:00:52 -0300

    Adding options to cookies

--- src/services/api.ts
+++ src/services/api.ts
@@ -46,13 +46,11 @@ api.interceptors.response.use(
                 path: "/",
                 secure: true,
                 sameSite: "none",
-                domain: "edpinheiro.dev",
               });
               setCookie(null, "appfin.refreshToken", refreshToken, {
                 path: "/",
                 secure: true,
                 sameSite: "none",
-                domain: "edpinheiro.dev",
               });
               failedRequestQueue.forEach((request) => request.onSuccess(token));
               failedRequestQueue = [];
